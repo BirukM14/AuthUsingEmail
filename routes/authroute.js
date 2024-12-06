9cb58d4a90618b16903039f0c7bf069719af5a3b
BirukM14 <gbiruka14@gmail.com>
2024-12-06 21:10:09 +0300

    check-auth

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { signup, verifyEmail, logout, login,forgotpassword } from "../controller/authcontroller.js";
-
+import { signup, verifyEmail, logout, login,forgotpassword, resetpassword,checkAuth } from "../controller/authcontroller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-
+router.get("/check-auth",verifyToken ,checkAuth);
 router.post("/signup",signup);
 router.patch("/verifyemail",verifyEmail);
 router.post("/logout",logout);
 router.post("/login",login);
 router.post("/forgotpass",forgotpassword);
-
+router.post("/reset-password/:token",resetpassword);
 export default router;
